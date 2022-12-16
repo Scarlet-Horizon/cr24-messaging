516a86fdf5c85c14ca670f7cca67080256df42cb
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-12-16 10:48:23 +0100

    Switch container start up order

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ cd messaging/ || exit 1
 docker container stop rabbit-con
 docker-compose up -d
 
-cd ../account-service/ || exit 2
-docker container stop account-api-con
+cd ../account-stat/ || exit 2
+docker container stop account-stat-con
 docker-compose up -d
 
-cd ../account-stat/ || exit 3
-docker container stop account-stat-con
+cd ../account-service/ || exit 3
+docker container stop account-api-con
 docker-compose up -d
 
 cd ../transaction/transaction-files/ || exit 4
@@ -19,5 +19,3 @@ docker-compose up -d
 cd ../transaction-service/ || exit 5
 docker container stop transaction-api-con
 docker-compose up -d
-
-cd ../../
