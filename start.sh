047e49bc045c41b854e654c47a3e6040fa2b03ad
David Slatinek <79467409+david-slatinek@users.noreply.github.com>
2022-12-16 10:16:45 +0100

    Add a script to start all services

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd messaging/ || exit 1
+docker container stop rabbit-con
+docker-compose up -d
+
+cd ../account-service/ || exit 2
+docker container stop acccunt-api-con
+docker-compose up -d
+
+cd ../account-stat/ || exit 3
+docker container stop account-stat-con
+docker-compose up -d
+
+cd ../transaction/transaction-files/ || exit 4
+docker container stop transaction-db-con
+docker-compose up -d
+
+cd ../transaction-service/ || exit 5
+docker container stop transaction-api-con
+docker-compose up -d
+
+cd ../../
